d619e59b2918c1ad77f2a85c2dafbc4cbd8380f3
dingxiangpeng <ding@qq.com>
2022-09-14 19:04:43 +0800

    add _.chunk

--- lodash/ilyaunchat-lodash.js
+++ lodash/ilyaunchat-lodash.js
@@ -1,4 +1,28 @@
 var ilyaunchat = {
+    chunk: function (array, size = 1) {
+        var resultAry = []
+        var itemAry = []
+        var count = 0
+
+        for (var i = 0; i < array.length; i++) {
+            count++
+            if (count <= size) {
+                itemAry.push(array[i])
+            } else if (count > size) {
+                count = 0
+                resultAry.push(itemAry.slice())
+                itemAry = []
+                itemAry.push(array[i])
+            }
+        }
+
+        if (itemAry.length) {
+            resultAry.push(itemAry.slice())
+        }
+
+        return resultAry
+    },
+
     difference: function (array, ...values) {
         var resultAry = []
 
