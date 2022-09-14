d89b3226ef039c0b73f3f7efea19e0bd0e1c0210
dingxiangpeng <ding@qq.com>
2022-09-14 17:19:55 +0800

    add _.difference

--- lodash/ilyaunchat-lodash.js
+++ lodash/ilyaunchat-lodash.js
@@ -1,3 +1,24 @@
 var ilyaunchat = {
+    difference: function (array, values) {
+        var resultAry = []
+        var map = {}
 
+        for (var i = 0; i < values.length; i++) {
+            if (!(values[i] in map)) {
+                map[values[i]] = 1
+            } else {
+                map[values[i]]++
+            }
+        }
+
+        for (var i = 0; i < array.length; i++) {
+            var val = ((map[array[i]]) || 0)
+
+            if (!val) {
+                resultAry.push(array[i])
+            }
+        }
+
+        return resultAry
+    },
 }
