55a54bf875d40ff10281e0ee0051aad55b8e0ef0
dingxiangpeng <ding@qq.com>
2022-09-14 17:32:54 +0800

    modify _.difference, support flatten operator, and support more params

--- lodash/ilyaunchat-lodash.js
+++ lodash/ilyaunchat-lodash.js
@@ -1,19 +1,21 @@
 var ilyaunchat = {
-    difference: function (array, values) {
+    difference: function (array, ...values) {
         var resultAry = []
+
         var map = {}
 
         for (var i = 0; i < values.length; i++) {
-            if (!(values[i] in map)) {
-                map[values[i]] = 1
-            } else {
-                map[values[i]]++
+            for (var j = 0; j < values[i].length; j++) {
+                if (!(values[i][j] in map)) {
+                    map[values[i][j]] = 1
+                } else {
+                    map[values[i][j]]++
+                }
             }
         }
 
         for (var i = 0; i < array.length; i++) {
             var val = ((map[array[i]]) || 0)
-
             if (!val) {
                 resultAry.push(array[i])
             }
